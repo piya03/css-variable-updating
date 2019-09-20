93102584c957fbe2e0774cee04a7f845d1b5e96e
piya03 <piyachaudhary50@gmail.com>
2019-09-20 20:21:40 +0530

    first commit

---
+++ README.markdown
@@ -0,0 +1,4 @@
+# update css variable with js
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/piya50/pen/YzKOeKP](https://codepen.io/piya50/pen/YzKOeKP).
+
+ 
---
+++ dist/script.js
@@ -0,0 +1,13 @@
+const inputs = document.querySelectorAll('.controls input')
+function update(){
+  console.log(this.value)
+  const suffex = this.dataset.sizing || '';
+  //console.log(suffex)
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suffex)
+ // console.log(this.dataset)
+}
+inputs.forEach((eachinput)=>{
+  eachinput.addEventListener('change', update)
+    eachinput.addEventListener('mousemove', update)
+
+})
